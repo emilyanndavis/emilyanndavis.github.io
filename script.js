04a6ef83f38014dfbefa6fe4cce9bab496d12e57
Emily Davis <emilyanndavis@gmail.com>
2016-06-18 14:27:54 -0600

    updated projects section to feature real projects rather than placeholder photos, tweaked layout, added js script

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	
+	$('.menu-link').on('click', function(event) {
+		event.preventDefault();
+		var target = this.hash;		
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 700, function() {
+			window.location.target = target;
+		});
+	});
+	
+	$('.collapsed').on('click', function() {
+		if ($('h1').hasClass('room-for-menu')) {
+			$('h1').animate(
+				{paddingTop: '40px'}, 500, function() {
+					$('h1').removeClass('room-for-menu');
+				});
+		}
+		else {		
+			$('h1').animate(
+				{paddingTop: '100px'}, 300, function() {
+					$('h1').addClass('room-for-menu');
+				});
+		}
+	});
+    
+		
+	$('.fa, .fcc-logo').on('mouseenter focusin', function () {
+		$(this).animate({opacity: 0.7}, 50);	
+        $(this).closest('div').find('p').slideDown();
+	});
+	
+	$('.fa, .fcc-logo').on('mouseleave focusout', function () {
+		$(this).animate({opacity: 1}, 50);	
+        $(this).closest('div').find('p').slideUp();
+	});
+	
+});
